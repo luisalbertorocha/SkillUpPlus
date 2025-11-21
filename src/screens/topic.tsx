b1299292ee140ec66de572c1b07a6ee9e2cafb7d
Luis Alberto <luisalberto-rocha@hotmail.com>
2025-11-21 05:58:25 -0300

    fix: consertado problema quando abria o topico direto pelo bottom nabigation sem escolher nenhum item pela primeira vez

--- src/screens/topic.tsx
+++ src/screens/topic.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { View, Text, ScrollView } from "react-native";
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { globalStyles } from "../styles/stylesheet";
-
-
+import { topics } from "../data/topics";
 
 export default function TopicScreen({ route }: any) {
-  const { title, content } = route.params;
+  const defaultTopic = topics[0];
+
+  const { title, content } = route?.params || {
+    title: defaultTopic.title,
+    content: defaultTopic.content,
+  };
 
   return (
     <View style={globalStyles.screenContainer}>
       <Text style={globalStyles.title}>{title}</Text>
 
       <ScrollView showsVerticalScrollIndicator={false}>
-        <Text style={[globalStyles.text, { lineHeight: 22 }]}>{content}</Text>
+        <Text style={[globalStyles.text, { lineHeight: 22 }]}>
+          {content}
+        </Text>
       </ScrollView>
     </View>
   );
